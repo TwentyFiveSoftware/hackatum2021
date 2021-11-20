96a09ce76ec4c6665a6594975a235a915845eaee
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-11-20 09:39:02 +0100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles/App.module.scss';
+
+const App = () => {
+    return (
+        <div className={styles.App}>
+            <h1>#hackaTUM</h1>
+        </div>
+    );
+};
+
+export default App;
